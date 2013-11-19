8b49bed410e1fb7ebf2688349c7ca5973c2d8924
apoc <kingk912@gmail.com>
2013-11-19 01:45:33 -0600

    cambie trim por replaceAll al imprimir resultados

--- src/AFNE.java
+++ src/AFNE.java
@@ -399,29 +399,26 @@ public class AFNE {
 //		return a;
 //	}
 	
-	public Set<String> accepted(String input, PrintWriter printer) {
-		HashSet<String> result = new HashSet<String>();
+	public Set<String> accepted(String input) {
+		Set<String> result = new HashSet<String>();
 		
 		for (int i = 0; i < input.length(); i++) {
-			accepted(input.substring(i), initialState, 0, result, printer);
+			accepted(input.substring(i), initialState, 0, result);
 		}
 		
 		return result;
 	}
 	
-	private void accepted(String input, int current, int i, HashSet<String> result, PrintWriter printer) {
-		boolean a = false;
-
+	private void accepted(String input, int current, int i, Set<String> result) {
 		for (int state : epsilonClosure(current)) {
 			if (finals.get(state)) {
 				result.add(input.substring(0, i));
-				return;
 			}
 		}
 		
 		for (int state : epsilonClosure(current)) {
 			for (int child : getTransitions(state, input.charAt(i))) {
-				accepted(input, child, i + 1, result, printer);
+				accepted(input, child, i + 1, result);
 			}
 		}
 	}
